976e286d30dcfd67b4e03bf1a2d9eebd670572c8
zjf_rpm <501039356@qq.com>
2021-10-23 10:31:40 +0800

    first commit

---
+++ src/test/java/com/rpm/pojo/UserTest.java
@@ -0,0 +1,8 @@
+package com.rpm.pojo;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("张三", "66666");
+        System.out.println(user);
+    }
+}
