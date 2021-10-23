58684b4d0be30f68a682a542ff001475efa1feef
zjf_rpm <501039356@qq.com>
2021-10-23 10:38:33 +0800

    third commit

--- src/main/java/com/rpm/service/impl/UserServiceImpl.java
+++ src/main/java/com/rpm/service/impl/UserServiceImpl.java
@@ -3,11 +3,16 @@ package com.rpm.service.impl;
 import com.rpm.pojo.User;
 import com.rpm.service.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     @Override
     public List<User> getUsers() {
-        return null;
+        return new ArrayList<User>(){{
+            add(new User("张三","asjhdg"));
+            add(new User("李四","sadas"));
+            add(new User("王五","werew"));
+        }};
     }
 }
