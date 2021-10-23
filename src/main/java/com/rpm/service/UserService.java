8b65cdb7621cd973dc66cbe9b1acf642955e0308
zjf_rpm <501039356@qq.com>
2021-10-23 10:37:00 +0800

    second commit

---
+++ src/main/java/com/rpm/service/UserService.java
@@ -0,0 +1,14 @@
+package com.rpm.service;
+
+import com.rpm.pojo.User;
+
+import java.util.List;
+
+/**
+ * 用户服务接口
+ */
+public interface UserService {
+
+    public List<User> getUsers();
+
+}
---
+++ src/main/java/com/rpm/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.rpm.service.impl;
+
+import com.rpm.pojo.User;
+import com.rpm.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+}
